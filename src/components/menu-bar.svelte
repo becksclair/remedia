2dd37055f9e4b1744faf0284818d36267b6d4efe
Rebecca Clair <rebecca@heliasar.com>
2024-10-17 16:49:37 +0200

    feat: refactor and implement design vision for menu bar, impl toggle dark mode

--- src/components/menu-bar.svelte
+++ src/components/menu-bar.svelte
@@ -1,43 +1,29 @@
 <script lang="ts">
+	import { toggleMode } from 'mode-watcher'
 	import * as Menubar from '$lib/components/ui/menubar'
 
-	import Sun from 'svelte-radix/Sun.svelte'
-	import Moon from 'svelte-radix/Moon.svelte'
-	import { toggleMode } from 'mode-watcher'
-
 	export let alwaysOnTop = false
+	let darkMode = false
 </script>
-
-<!-- <Button on:click={toggleMode} variant="outline" size="icon">
-	<Sun
-	class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
-	/>
-	<Moon
-	class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
-	/>
-	<span class="sr-only">Toggle theme</span>
-</Button> -->
 
 <Menubar.Root>
 	<Menubar.Menu>
 		<Menubar.Trigger>File</Menubar.Trigger>
 		<Menubar.Content>
 			<Menubar.Item>
-				New Tab <Menubar.Shortcut>⌘T</Menubar.Shortcut>
+				Add URL... <Menubar.Shortcut>⌘A</Menubar.Shortcut>
 			</Menubar.Item>
 			<Menubar.Item>
-				New Window <Menubar.Shortcut>⌘N</Menubar.Shortcut>
+				Add from clipboard <Menubar.Shortcut>⌘V</Menubar.Shortcut>
 			</Menubar.Item>
-			<Menubar.Item>New Incognito Window</Menubar.Item>
+			<Menubar.Item>
+				Copy all URLs <Menubar.Shortcut>⇧⌘C</Menubar.Shortcut>
+			</Menubar.Item>
+			<Menubar.Item>
+				Export URL list... <Menubar.Shortcut>⌘E</Menubar.Shortcut>
+			</Menubar.Item>
 			<Menubar.Separator />
-			<Menubar.Sub>
-				<Menubar.SubTrigger>Share</Menubar.SubTrigger>
-				<Menubar.SubContent>
-					<Menubar.Item>Email link</Menubar.Item>
-					<Menubar.Item>Messages</Menubar.Item>
-					<Menubar.Item>Notes</Menubar.Item>
-				</Menubar.SubContent>
-			</Menubar.Sub>
+			<Menubar.Item>Preferences...</Menubar.Item>
 			<Menubar.Separator />
 			<Menubar.Item>
 				Exit <Menubar.Shortcut>⌘Q</Menubar.Shortcut>
@@ -45,45 +31,31 @@
 		</Menubar.Content>
 	</Menubar.Menu>
 	<Menubar.Menu>
-		<Menubar.Trigger>Edit</Menubar.Trigger>
+		<Menubar.Trigger>Find</Menubar.Trigger>
 		<Menubar.Content>
-			<Menubar.Sub>
-				<Menubar.SubTrigger>Find</Menubar.SubTrigger>
-				<Menubar.SubContent>
-					<Menubar.Item>Search the web</Menubar.Item>
-					<Menubar.Separator />
-					<Menubar.Item>Find...</Menubar.Item>
-					<Menubar.Item>Find Next</Menubar.Item>
-					<Menubar.Item>Find Previous</Menubar.Item>
-				</Menubar.SubContent>
-			</Menubar.Sub>
-			<Menubar.Separator />
-			<Menubar.Item>
-				Cut <Menubar.Shortcut>⌘X</Menubar.Shortcut>
-			</Menubar.Item>
-			<Menubar.Item>
-				Copy <Menubar.Shortcut>⌘C</Menubar.Shortcut>
-			</Menubar.Item>
-			<Menubar.Item>
-				Paste <Menubar.Shortcut>⌘V</Menubar.Shortcut>
-			</Menubar.Item>
+			<Menubar.Item>Search the web</Menubar.Item>
+			<Menubar.Item>Find...</Menubar.Item>
 		</Menubar.Content>
 	</Menubar.Menu>
 	<Menubar.Menu>
 		<Menubar.Trigger>View</Menubar.Trigger>
 		<Menubar.Content>
-			<Menubar.CheckboxItem bind:checked={alwaysOnTop}>Always on top</Menubar.CheckboxItem>
+			<Menubar.CheckboxItem bind:checked={alwaysOnTop}>
+				Always on top <Menubar.Shortcut>⇧⌘O</Menubar.Shortcut>
+			</Menubar.CheckboxItem>
+			<Menubar.CheckboxItem bind:checked={darkMode} on:click={toggleMode}>
+				Dark mode <Menubar.Shortcut>⇧⌘D</Menubar.Shortcut>
+			</Menubar.CheckboxItem>
 			<Menubar.Separator />
 			<Menubar.Item inset>
 				Reload <Menubar.Shortcut>⌘R</Menubar.Shortcut>
 			</Menubar.Item>
-			<Menubar.Item inset>
-				Force Reload <Menubar.Shortcut>⇧⌘R</Menubar.Shortcut>
-			</Menubar.Item>
-			<Menubar.Separator />
-			<Menubar.Item inset>Toggle Fullscreen</Menubar.Item>
-			<Menubar.Separator />
-			<Menubar.Item inset>Hide Sidebar</Menubar.Item>
+		</Menubar.Content>
+	</Menubar.Menu>
+	<Menubar.Menu>
+		<Menubar.Trigger>Help</Menubar.Trigger>
+		<Menubar.Content>
+			<Menubar.Item inset>About...</Menubar.Item>
 		</Menubar.Content>
 	</Menubar.Menu>
 </Menubar.Root>
