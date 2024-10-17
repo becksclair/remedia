b9f0544f3afb7edbcad9f1f64ef5b8af26b90a4e
Rebecca Clair <rebecca@heliasar.com>
2024-10-17 16:09:45 +0200

    chore: cleanup rust lib

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,7 +1,5 @@
-// use downloader::get_media_info;
 use remedia::quit;
 use tauri::Manager;
-// use tauri_plugin_shell::ShellExt;
 
 mod downloader;
 mod remedia;
@@ -26,12 +24,6 @@ pub fn run() {
         }));
     }
 
-    // #[cfg(debug_assertions)]
-    // {
-    //     let devtools = tauri_plugin_devtools::init(); // initialize the plugin as early as possible
-    //     builder = builder.plugin(devtools); // initialize the plugin as early as possible
-    // }
-
     builder = builder.setup(|_app| {
         // #[cfg(debug_assertions)] // only include this code on debug builds
         // app.get_webview_window("main").unwrap().open_devtools();
@@ -47,24 +39,4 @@ pub fn run() {
     builder
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
-
-    // #[cfg(debug_assertions)]
-    // let devtools = tauri_plugin_devtools::init(); // initialize the plugin as early as possible
-    // tauri::Builder::default()
-    //     .plugin(tauri_plugin_single_instance::init(|app, args, cwd| {}))
-    //     .plugin(tauri_plugin_fs::init())
-    //     .plugin(tauri_plugin_dialog::init())
-    //     .plugin(tauri_plugin_clipboard_manager::init())
-    //     .plugin(tauri_plugin_shell::init())
-    //     // .plugin(devtools)
-    //     // .setup(|app| {
-    //     //     #[cfg(debug_assertions)] // only include this code on debug builds
-    //     //     // app.get_webview_window("main").unwrap().open_devtools();
-    //     //     Ok(())
-    //     // })
-    //     .invoke_handler(tauri::generate_handler![get_media_info])
-    //     .invoke_handler(tauri::generate_handler![download])
-    //     .invoke_handler(tauri::generate_handler![quit])
-    //     .run(tauri::generate_context!())
-    //     .expect("error while running tauri application");
 }
