42f68eaae2781d3a42de649bcb6e1b8dc9f35a2b
Rebecca Clair <rebecca@heliasar.com>
2024-10-17 16:07:44 +0200

    chore: reformat p-label component

--- src/components/p-label.svelte
+++ src/components/p-label.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
-    import { cn } from "$lib/utils.js";
+	import { cn } from '$lib/utils.js'
 
-    export let className = "";
+	export let className = ''
 </script>
 
-<p class={cn("text-sm font-medium leading-none py-3", className)}>
-    Select the location where you want to save the downloaded files:
-    <slot />
+<p class={cn('text-sm font-medium leading-none py-3', className)}>
+	Select the location where you want to save the downloaded files:
+	<slot />
 </p>
